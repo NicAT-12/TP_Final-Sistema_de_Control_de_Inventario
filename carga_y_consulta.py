49bc1546751b77430828904ae28c105e8bb6d59c
NicAT-12 <nicotissoni12@gmail.com>
2026-09-11 22:09:39 -0300

    Se completó la función cargar_herramientas

--- carga_y_consulta.py
+++ carga_y_consulta.py
@@ -9,3 +9,32 @@ def cargar_herramientas(inventario):
             break
         except ValueError as only_num:
             print(f"Error: {only_num}")
+
+    for _ in range(cantidad_de_herramientas_ingresadas):
+        while True:
+            try:
+                nombre_herramienta_ingresada = input("Ingrese el nombre de la herramienta: ")
+
+                if not nombre_herramienta_ingresada.strip():
+                    raise ValueError("Ingrese un nombre válido")
+
+                for herramienta in inventario:
+                    if herramienta['herramienta'] == nombre_herramienta_ingresada:
+                        raise ValueError(f"{herramienta['herramienta']} ya existe en el inventario")
+
+                break
+            except ValueError as error:
+                print(f"Error: {error}")
+
+        while True:
+            try:
+                stock_herramienta_ingresada = int(input("Ingrese el stock de la herramienta: "))
+
+                if stock_herramienta_ingresada < 0:
+                    raise ValueError("El stock no puede ser negativo")
+
+                break
+            except ValueError as error:
+                print(f"Error: {error}")
+
+    inventario.append({'herramienta': nombre_herramienta_ingresada, 'cantidad': stock_herramienta_ingresada})
